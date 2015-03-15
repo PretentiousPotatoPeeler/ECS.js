9b7960af21f32c11ca96e33c784cfff6c70e9ece
PepijnCompaan <pepijncompaan@hotmail.com>
2015-03-15 21:47:22 +0100

    ECS.js-3 Make more components: Made image component (view and model)

--- components.js
+++ components.js
@@ -1,45 +1,57 @@
-
-
 var loc = {
-	name: "location",
-	x: 0,
-	y: 0,
-	init: function(x, y) {
-		this.x = x;
-		this.y = y;
-		return this;
-	}
+    name: "location",
+    x: 0,
+    y: 0,
+    init: function (x, y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
 }
 
-function Squere(p, x, y, width, height){
-	this.name = "squere";
+function Squere(p, x, y, width, height) {
+    this.name = "squere";
     this.parent = p;
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 }
 
-function DisplaySolid(p){
-	this.name = "displaySolid";
+function DisplaySolid(p) {
+    this.name = "displaySolid";
     this.parent = p;
-	this.color = "#00A";
-	this.squere = this.parent.model["squere"];
-	this.draw = function() {
-		canvas.fillStyle = this.color;
-		canvas.fillRect(this.squere.x, this.squere.y, this.squere.width, this.squere.height);
-	}
+    this.color = "#00A";
+    this.squere = this.parent.model["squere"];
+    this.draw = function () {
+        canvas.fillStyle = this.color;
+        canvas.fillRect(this.squere.x, this.squere.y, this.squere.width, this.squere.height);
+    }
 }
 
 function ArrowMove(p) {
-	this.name = "arrowMove";
+    this.name = "arrowMove";
     this.category = "controller";
     this.parent = p;
-	this.squere = this.parent.model["squere"];
-	this.update = function() {
-		if (Key.isDown(Key.UP)) this.squere.y--;
-		if (Key.isDown(Key.LEFT)) this.squere.x--;
-		if (Key.isDown(Key.DOWN)) this.squere.y++;
-		if (Key.isDown(Key.RIGHT)) this.squere	.x++;
-	}
+    this.squere = this.parent.model["squere"];
+    this.update = function () {
+        if (Key.isDown(Key.UP)) this.squere.y--;
+        if (Key.isDown(Key.LEFT)) this.squere.x--;
+        if (Key.isDown(Key.DOWN)) this.squere.y++;
+        if (Key.isDown(Key.RIGHT)) this.squere.x++;
+    }
+}
+
+function ImageModel(p, path) {
+    this.name = "imageModel";
+    this.imageObj = new Image();
+    this.imageObj.src = path;
+    this.parent = p;
+}
+
+function DisplayImage(p){
+    console.log(p.model["imageModel"]);
+    this.draw = function () {
+        canvas.drawImage(p.model["imageModel"].imageObj, p.model["squere"].x, p.model["squere"].y);
+    }
 }
