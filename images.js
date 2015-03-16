e4fc377cc7440fcd9ec5308d4431eeadbbc5a132
PepijnCompaan <pepijncompaan@hotmail.com>
2015-03-16 21:18:53 +0100

    Added some more grass textures.

--- images.js
+++ images.js
@@ -1,14 +1,20 @@
 var img_idiot = new Image();
 img_idiot.src = "img/idiot.gif";
 
-var img_grass = new Image();
-img_grass.src = "img/blocks/grass.png";
-
 var img_stone = new Image();
 img_stone.src = "img/blocks/stone.png";
 
 var img_tree = new Image();
 img_tree.src = "img/blocks/tree.png";
 
-var img_grass = new Image();
-img_grass.src = "img/blocks/grass.png";
+var img_grass0 = new Image();
+img_grass0.src = "img/blocks/grass_0.png";
+
+var img_grass1 = new Image();
+img_grass1.src = "img/blocks/grass_1.png";
+
+var img_grass2 = new Image();
+img_grass2.src = "img/blocks/grass_2.png";
+
+var img_grass3 = new Image();
+img_grass3.src = "img/blocks/grass_3.png";
