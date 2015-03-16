690b6d90316bc72fa36ce9b60d1d3ea65c21b16b
PepijnCompaan <pepijncompaan@hotmail.com>
2015-03-16 01:37:17 +0100

    Reformat all code. Should do that before each commit from now on.

--- entity.js
+++ entity.js
@@ -1,30 +1,30 @@
-var Entity = function(name) {
-	this.name = name;
-	this.model = {};
-	this.view = {};
+var Entity = function (name) {
+    this.name = name;
+    this.model = {};
+    this.view = {};
     this.controller = {};
 
-	this.addModel = function(component){
-		this.model[component.name] = component;
-	};
+    this.addModel = function (component) {
+        this.model[component.name] = component;
+    };
 
-	this.getModel = function(component){
-		return this.model[component];
-	};
+    this.getModel = function (component) {
+        return this.model[component];
+    };
 
-	this.addView = function(component){
-		this.view[component.name] = component;
-	};
+    this.addView = function (component) {
+        this.view[component.name] = component;
+    };
 
-	this.getView = function(component){
-		return this.view[component];
-	};
+    this.getView = function (component) {
+        return this.view[component];
+    };
 
-    this.addController = function(component){
-		this.controller[component.name] = component;
-	};
+    this.addController = function (component) {
+        this.controller[component.name] = component;
+    };
 
-	this.getController = function(component){
-		return this.controller[component];
-	};
+    this.getController = function (component) {
+        return this.controller[component];
+    };
 }
